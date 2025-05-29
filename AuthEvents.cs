fd9b5c39666419893ce10bc4a8be15c310b27a24
DinkarKPatel <dinkar@wizapp.in>
2025-05-29 11:17:31 +0530

    Add files via upload

---
+++ AuthEvents.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Primitives;
+using Microsoft.Net.Http.Headers;
+using System.Data;
+using System.Data.SqlClient;
+using System.IdentityModel.Tokens.Jwt;
+using System.IO;
+using System.Net;
+using System.Web;
+using RestWizclipService.Models;
+
+namespace RestWizclipService
+{
+    public class AuthEventsHandler : JwtBearerEvents
+    {
+        private const string BearerPrefix = "Bearer";
+
+        private AuthEventsHandler() => OnMessageReceived = MessageReceivedHandler;
+
+        /// <summary>
+        /// Gets single available instance of <see cref="AuthEventsHandler"/>
+        /// </summary>
+        public static AuthEventsHandler Instance { get; } = new AuthEventsHandler();
+
+
+        private Task MessageReceivedHandler(MessageReceivedContext context)
+        {
+
+
+            if (context.Request.Headers.TryGetValue("Authorization", out StringValues headerValue))
+            {
+                string token = headerValue;
+
+                var bearerToken = context.Request.Headers[HeaderNames.Authorization].ToString().Replace("Bearer ", "");
+
+                if (!string.IsNullOrEmpty(token))
+                {
+
+                    var handler = new JwtSecurityTokenHandler();
+                    var jsonToken = handler.ReadToken(bearerToken);
+                    var jwtSecurityToken = jsonToken as JwtSecurityToken;
+
+
+                    var claims = jwtSecurityToken.Claims.ToList();
+
+                    int nClaims = claims.Count();
+
+                    string[,] TokenInfo = new string[nClaims, 2];
+                    int n = 0;
+                    foreach (var claim in claims)
+                    {
+                        if (claim.Type.ToUpper() == "ROLECODE")
+                            context.Request.HttpContext.Session.SetString("roleCode", claim.Value);
+                        else
+                        if (claim.Type.ToUpper() == "USERID")
+                            context.Request.HttpContext.Session.SetString("userId", claim.Value);
+                        else
+                        if (claim.Type.ToUpper() == "TOKENTYPE")
+                            context.Request.HttpContext.Session.SetString("tokenType", claim.Value);
+                        else
+                        if (claim.Type.ToUpper() == "APIACCESS")
+                            context.Request.HttpContext.Session.SetString("apiAccess", claim.Value);
+                        else
+                        if (claim.Type.ToUpper() == "APIGROUPCODE")
+                            context.Request.HttpContext.Session.SetString("apiGroupCode", claim.Value);
+                        else
+                        if (claim.Type.ToUpper() == "WIZCLIPGROUPCODE")
+                            context.Request.HttpContext.Session.SetString("wizclipGroupCode", claim.Value);
+
+                    }
+                }
+
+
+                context.Token = bearerToken;
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ myCustomMiddleware.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Threading.Tasks;
+using RestWizclipService.Models;
+
+
+namespace RestWizclipService.Middlewares
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class myCustomMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public IServiceProvider _serviceProvider;
+        public myCustomMiddleware(RequestDelegate next, IConfiguration _config)
+        {
+            _next = next;
+            AppConfigModel.globalConnConfig = _config;
+        }
+
+        public async Task Invoke(HttpContext httpContext, IServiceProvider serviceProvider)
+        {
+
+            string apiFullName = "";
+            var controllerName = httpContext.GetRouteData().Values["controller"];
+            var actionName = httpContext.GetRouteData().Values["action"];
+
+            if (controllerName is object && actionName is object)
+                apiFullName = controllerName.ToString().ToUpper() + "." + actionName.ToString().ToUpper();
+
+
+            httpContext.Session.SetString("apiFullName", apiFullName);
+
+            var GroupCode = httpContext.Request.Query.FirstOrDefault(q => string.Equals(q.Key, "Groupcode", StringComparison.OrdinalIgnoreCase)).Value;
+
+            if (!string.IsNullOrEmpty(GroupCode) && (apiFullName == "AUTH.AUTHENTICATEUSER" || controllerName.ToString().ToUpper() == "WEBSOCKET"))
+            {
+                httpContext.Session.SetString("apiGroupCode", GroupCode.ToString());
+                //sessionVariableService.apiGroupCode = GroupCode.ToString();
+            }
+
+            string tokenType = httpContext.Session.GetString("tokenType");
+
+            if (tokenType == "2" && apiFullName != "TOKEN.REISSUEACCESSTOKEN"
+                && apiFullName != "AUTH.GETUSERS" && apiFullName != "AUTH.AUTHENTICATEUSER")
+            {
+
+                HttpResponseMessage response = new HttpResponseMessage();
+
+                httpContext.Response.StatusCode = StatusCodes.Status403Forbidden;
+                await httpContext.Response.WriteAsync("API call with Refresh token is not allowed...");
+
+                return;
+            }
+
+            if (tokenType == "1" && apiFullName == "TOKEN.REISSUEACCESSTOKEN")
+            {
+
+                HttpResponseMessage response = new HttpResponseMessage();
+
+                httpContext.Response.StatusCode = StatusCodes.Status403Forbidden;
+                await httpContext.Response.WriteAsync("New Access token cannot be issued with Bearer access token...");
+
+                return;
+            }
+
+            _serviceProvider = serviceProvider;
+            await _next(httpContext);
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class CustomMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseCustomMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<myCustomMiddleware>();
+        }
+    }
+}
